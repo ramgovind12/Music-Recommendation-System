b65ac8211b006bee2a179265a83ad796a95b7858
Ram Govind V <ramgovindvi.in@gmail.com>
2023-09-25 22:09:07 +0530

    streamlit done

---
+++ app.py
@@ -0,0 +1,55 @@
+import pickle
+import streamlit as st
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+CLIENT_ID = "0e69da68570c4e32bdce68159d1ddd58"
+CLIENT_SECRET = "aa74038696be405fa9ab22d97ef55568"
+
+client_credentials = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials)
+
+def get_song_album_cover_url(song_name, artist_name):
+    query = f"track:{song_name} artist:{artist_name}"
+    results = sp.search(q=query, type="track")
+
+    if results and results["tracks"]["items"]:
+        track = results["tracks"]["items"][0]
+        album_cover_url = track["album"]["images"][0]["url"]
+        print(album_cover_url)
+        return album_cover_url
+    else:
+        return "https://i.postimg.cc/0QNxYz4V/social.png"
+    
+
+def recommend(song):
+    index = music[music['song'] == song].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_music_names = []
+    recommended_music_posters = []
+    for i in distances[1:11]:
+        artist = music.iloc[i[0]].artist
+        print(artist)
+        print(music.iloc[i[0]].song)
+        recommended_music_posters.append(get_song_album_cover_url(music.iloc[i[0]].song, artist))
+        recommended_music_names.append(music.iloc[i[0]].song)
+
+    return recommended_music_names, recommended_music_posters
+
+st.header('Music Recommendation System')
+music = pickle.load(open('df.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+music_list = music['song'].values
+selected_movies = st.selectbox("Search or Select a song",music_list)
+
+if st.button('Show Recommendation'):
+    recommended_music_names, recomended_music_posters = recommend(selected_movies)
+
+    for row in range(2):
+        cols = st.columns(5)
+        for i in range(5):
+            with cols[i]:
+                index = i + row * 5
+                st.text(recommended_music_names[index])
+                st.image(recomended_music_posters[index])
